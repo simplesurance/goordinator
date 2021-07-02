ed41524e0376bf41857ead627f49d749ab1b3018
Fabian Holler <fabian.holler@simplesurance.de>
2021-07-02 11:40:19 +0200

    autoupdater: add helper to write strings to http.ResponseWriter
    add a helper struct that wraps a http.ResponseWriter and provides a method to
    write a string instead to it and logs a message on error

--- internal/autoupdate/httphandler.go
+++ internal/autoupdate/httphandler.go
@@ -9,9 +9,36 @@ import (
 	"go.uber.org/zap"
 )
 
-func (a *Autoupdater) HTTPHandlerList(resp http.ResponseWriter, req *http.Request) {
+type httpRespWriter struct {
+	http.ResponseWriter
+	logger *zap.Logger
+}
+
+func newHTTPRespWriter(logger *zap.Logger, resp http.ResponseWriter) *httpRespWriter {
+	return &httpRespWriter{
+		ResponseWriter: resp,
+		logger:         logger,
+	}
+}
+
+// WriteStr writes a string to the http response write.
+// If an error happens, it is logged with info priority and false is returned.
+// If it suceeded true is returned.
+func (rw *httpRespWriter) WriteStr(str string) (wasSuccessful bool) {
+	_, err := rw.ResponseWriter.Write([]byte(str))
+	if err != nil {
+		rw.logger.Info("sending http response failed", zap.Error(err))
+		return false
+	}
+
+	return true
+}
+
+func (a *Autoupdater) HTTPHandlerList(respWr http.ResponseWriter, req *http.Request) {
 	var result strings.Builder
 	// TODO: write to resp directly instead of to strings.Builder
+
+	resp := newHTTPRespWriter(a.logger, respWr)
 
 	resp.Header().Add("Content-Type", "text/plain")
 
@@ -19,24 +46,20 @@ func (a *Autoupdater) HTTPHandlerList(resp http.ResponseWriter, req *http.Reques
 	defer a.queuesLock.Unlock()
 
 	if len(a.queues) == 0 {
-		_, err := resp.Write([]byte("no pull-requests queued for updates\n"))
-		if err != nil {
-			a.logger.Info(
-				"writing to http-response writer failed",
-				zap.Error(err),
-			)
-		}
-
+		resp.WriteStr("no pull-requests queued for updates\n")
 		return
 	}
 
 	for _, queue := range a.queues {
-		result.WriteString(fmt.Sprintf(
+		success := resp.WriteStr(fmt.Sprintf(
 			"Base: %s/%s %s\n",
 			queue.baseBranch.RepositoryOwner,
 			queue.baseBranch.Repository,
 			queue.baseBranch.Branch,
 		))
+		if !success {
+			return
+		}
 
 		var i int
 		queue.active.Foreach(func(pr *PullRequest) bool {
@@ -55,11 +78,5 @@ func (a *Autoupdater) HTTPHandlerList(resp http.ResponseWriter, req *http.Reques
 		}
 	}
 
-	_, err := resp.Write([]byte(result.String()))
-	if err != nil {
-		a.logger.Info(
-			"writing to http-response writer failed",
-			zap.Error(err),
-		)
-	}
+	resp.WriteStr(result.String())
 }
