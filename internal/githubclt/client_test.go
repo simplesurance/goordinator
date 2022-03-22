4d07a8ec3e1656c3ae24e091d41847126a18b219
Fabian Holler <fabian.holler@simplesurance.de>
2022-03-22 13:24:17 +0100

    githubclient: retry graphl queries on 5xx errors
    When querying the github graphql endpoint, return a RetryableError if a 5xx
    error happened.
    These can be temporary errors on github side.

---
+++ internal/githubclt/client_test.go
@@ -0,0 +1,38 @@
+package githubclt
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/shurcooL/githubv4"
+	"github.com/simplesurance/goordinator/internal/goorderr"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+)
+
+func TestWrapRetryableErrorsGraphql(t *testing.T) {
+	t.Cleanup(zap.ReplaceGlobals(zaptest.NewLogger(t).Named(t.Name())))
+
+	// is the same then in vendor/github.com/shurcooL/graphql/graphql.go do()
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(503)
+	}))
+
+	t.Cleanup(srv.Close)
+
+	clt := Client{
+		logger:     zap.L(),
+		graphQLClt: githubv4.NewEnterpriseClient(srv.URL, srv.Client()),
+	}
+
+	s, err := clt.ReadyForMergeStatus(context.Background(), "test", "test", 123)
+	require.Error(t, err)
+	assert.Nil(t, s)
+
+	var retryableErr *goorderr.RetryableError
+	assert.ErrorAs(t, err, &retryableErr)
+}
